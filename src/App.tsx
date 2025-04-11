34791437c3b09e2c24a9c6145057339a4ffb68e4
Rudramani1 <rudramaniupadhyay651@gmail.com>
2025-04-11 21:18:54 +0530

    added Join quiz

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -11,6 +10,7 @@ import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import Quizzes from "./pages/Quizzes";
 import Flowcharts from "./pages/Flowcharts";
+import JoinQuiz from "./pages/JoinQuiz";
 
 
 const queryClient = new QueryClient();
@@ -29,6 +29,9 @@ const App = () => {
                 <Route path="/flashcards" element={<Flashcards />} />
                 <Route path="/quizzes" element={<Quizzes />} />
                 <Route path="/flowcharts" element={<Flowcharts />} />
+                <Route path="/joinquiz" element={<JoinQuiz />} />
+                <Route path="/joinquiz/:sessionId" element={<JoinQuiz />} />
+                <Route path="/play/:sessionId" element={<NotFound />} />
                 <Route path="*" element={<NotFound />} />
               </Routes>
             </Layout>
